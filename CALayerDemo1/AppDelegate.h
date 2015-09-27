d1d469e209cfcfd3ffc2dc2b065d7ff141df57f3
gyf5267 <gyf5267218@163.com>
2015-09-27 21:44:52 +0800

    Initial Commit

---
+++ CALayerDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CALayerDemo1
+//
+//  Created by nate on 15/9/27.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
